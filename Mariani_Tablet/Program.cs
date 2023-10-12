4e7e4ab0d2a1d4279eeca5e3490c89bd0e94c2d6
ludddo <mariani.ludovico.studente@itispaleocapa.it>
2023-10-12 14:59:16 +0200

    Stampa Tablet Minino e Tablet Massimo

--- Mariani_Tablet/Program.cs
+++ Mariani_Tablet/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -31,7 +32,7 @@ namespace Mariani_Tablet
 
                 Console.Clear();
             }
-            
+
             Tablet tablet1 = new Tablet(marca[0], velocita[0], dimensione[0], durata[0]);
             Tablet tablet2 = new Tablet(marca[1], velocita[1], dimensione[1], durata[1]);
             Tablet tablet3 = new Tablet(marca[2], velocita[2], dimensione[2], durata[2]);
@@ -46,29 +47,62 @@ namespace Mariani_Tablet
             punteggioMedio[3] = (tablet4.Punteggio()[0] + tablet4.Punteggio()[1] + tablet4.Punteggio()[2]) / 3;
             punteggioMedio[4] = (tablet5.Punteggio()[0] + tablet5.Punteggio()[1] + tablet5.Punteggio()[2]) / 3;
 
-            int tabletMigliore = 0, tabletPeggiore = 0;
+
+            int indirizzoMin = 0, indirizzoMax = 0;
+            double valMax = 0, valMin = 10000;
 
             for (int i = 0; i < 5; i++)
             {
-                for (int j = 0; j < 5; j++)
+                if (punteggioMedio[i] > valMax)
                 {
-                    if (punteggioMedio[i] > punteggioMedio[j])
-                    {
-                        tabletMigliore = i;
-                    }
-                    if (punteggioMedio[i] < punteggioMedio[j])
-                    {
-                        tabletPeggiore = j;
-                    }
+                    indirizzoMax = i;
+                    valMax = punteggioMedio[i];
+                }
+                if (punteggioMedio[i] < valMin)
+                {
+                    indirizzoMin = i;
+                    valMin = punteggioMedio[i];
                 }
             }
 
-            Console.WriteLine(tabletMigliore);
-            Console.WriteLine(tabletPeggiore);
 
-            Console.WriteLine("Il tablet peggiore è:\nMarca: " + "");
-            
+            switch (indirizzoMin)
+            {
+                case 0:
+                    Console.WriteLine("Il tablet peggiore ha effettuato un punteggio di " + Math.Round(punteggioMedio[0]) + "ed é:\nMarca: " + tablet1.getMarca() + "\nVelocitá: " + tablet1.getVelocita() + "\nDimensione: " + tablet1.getDimensione() + "\nDurata: " + tablet1.getDurata() + "\n\n");
+                    break;
+                case 1:
+                    Console.WriteLine("Il tablet peggiore ha effettuato un punteggio di " + Math.Round(punteggioMedio[1]) + " ed é:\nMarca: " + tablet2.getMarca() + "\nVelocitá: " + tablet2.getVelocita() + "\nDimensione: " + tablet2.getDimensione() + "\nDurata: " + tablet2.getDurata() + "\n\n");
+                    break;
+                case 2:
+                    Console.WriteLine("Il tablet peggiore ha effettuato un punteggio di " + Math.Round(punteggioMedio[2]) + " ed é:\nMarca: " + tablet3.getMarca() + "\nVelocitá: " + tablet3.getVelocita() + "\nDimensione: " + tablet3.getDimensione() + "\nDurata: " + tablet3.getDurata() + "\n\n");
+                    break;
+                case 3:
+                    Console.WriteLine("Il tablet peggiore ha effettuato un punteggio di " + Math.Round(punteggioMedio[3]) + " ed é:\nMarca: " + tablet4.getMarca() + "\nVelocitá: " + tablet4.getVelocita() + "\nDimensione: " + tablet4.getDimensione() + "\nDurata: " + tablet4.getDurata() + "\n\n");
+                    break;
+                case 4:
+                    Console.WriteLine("Il tablet peggiore ha effettuato un punteggio di " + Math.Round(punteggioMedio[4]) + " ed é:\nMarca: " + tablet5.getMarca() + "\nVelocitá: " + tablet5.getVelocita() + "\nDimensione: " + tablet5.getDimensione() + "\nDurata: " + tablet5.getDurata() + "\n\n");
+                    break;
+            }
 
+            switch (indirizzoMax)
+            {
+                case 0:
+                    Console.WriteLine("Il tablet migliore ha effettuato un punteggio di " + Math.Round(punteggioMedio[0]) + "ed é:\nMarca: " + tablet1.getMarca() + "\nVelocitá: " + tablet1.getVelocita() + "\nDimensione: " + tablet1.getDimensione() + "\nDurata: " + tablet1.getDurata() + "\n\n");
+                    break;
+                case 1:
+                    Console.WriteLine("Il tablet migliore ha effettuato un punteggio di " + Math.Round(punteggioMedio[1]) + " ed é:\nMarca: " + tablet2.getMarca() + "\nVelocitá: " + tablet2.getVelocita() + "\nDimensione: " + tablet2.getDimensione() + "\nDurata: " + tablet2.getDurata() + "\n\n");
+                    break;
+                case 2:
+                    Console.WriteLine("Il tablet migliore ha effettuato un punteggio di " + Math.Round(punteggioMedio[2]) + " ed é:\nMarca: " + tablet3.getMarca() + "\nVelocitá: " + tablet3.getVelocita() + "\nDimensione: " + tablet3.getDimensione() + "\nDurata: " + tablet3.getDurata() + "\n\n");
+                    break;
+                case 3:
+                    Console.WriteLine("Il tablet migliore ha effettuato un punteggio di " + Math.Round(punteggioMedio[3]) +" ed é:\nMarca: " + tablet4.getMarca() + "\nVelocitá: " + tablet4.getVelocita() + "\nDimensione: " + tablet4.getDimensione() + "\nDurata: " + tablet4.getDurata() + "\n\n");
+                    break;
+                case 4:
+                    Console.WriteLine("Il tablet migliore ha effettuato un punteggio di " + Math.Round(punteggioMedio[4]) + " ed é:\nMarca: " + tablet5.getMarca() + "\nVelocitá: " + tablet5.getVelocita() + "\nDimensione: " + tablet5.getDimensione() + "\nDurata: " + tablet5.getDurata() + "\n\n");
+                    break;
+            }
         }
     }
 }
