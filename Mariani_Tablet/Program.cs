75e56f83347efff5b3d2dddd5b5db2ddab408e8e
ludo <mariani.ludovico.studente@itispaleocapa.it>
2023-10-12 10:43:55 +0200

    Calcolo Tablet Migliore e Peggiore

--- Mariani_Tablet/Program.cs
+++ Mariani_Tablet/Program.cs
@@ -26,10 +26,30 @@ namespace Mariani_Tablet
                 Console.WriteLine("Inserire la dimensione del " + i + " tablet");
                 dimensione[i] = int.Parse(Console.ReadLine());
 
-                
+                Console.WriteLine("Inserire la durata del " + i + " tablet");
+                durata[i] = double.Parse(Console.ReadLine());
             }
-         
             
+            Tablet tablet1 = new Tablet(marca[0], velocita[0], dimensione[0], durata[0]);
+            Tablet tablet2 = new Tablet(marca[1], velocita[1], dimensione[1], durata[1]);
+            Tablet tablet3 = new Tablet(marca[2], velocita[2], dimensione[2], durata[2]);
+            Tablet tablet4 = new Tablet(marca[3], velocita[3], dimensione[3], durata[3]);
+            Tablet tablet5 = new Tablet(marca[4], velocita[4], dimensione[4], durata[4]);
+
+            double[] punteggioMedio = new double[5];
+
+            punteggioMedio[0] = (tablet1.Punteggio()[0] + tablet1.Punteggio()[1] + tablet1.Punteggio()[2]) / 3;
+            punteggioMedio[1] = (tablet2.Punteggio()[0] + tablet2.Punteggio()[1] + tablet2.Punteggio()[2]) / 3;
+            punteggioMedio[2] = (tablet3.Punteggio()[0] + tablet3.Punteggio()[1] + tablet3.Punteggio()[2]) / 3;
+            punteggioMedio[3] = (tablet4.Punteggio()[0] + tablet4.Punteggio()[1] + tablet4.Punteggio()[2]) / 3;
+            punteggioMedio[4] = (tablet5.Punteggio()[0] + tablet5.Punteggio()[1] + tablet5.Punteggio()[2]) / 3;
+
+            Array.Sort(punteggioMedio);
+            
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(punteggioMedio[i]);
+            }
         }
     }
 }
